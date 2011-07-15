34f90e7f4de2084a72ebbe0707a6c10c8164ee36
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-07-15 11:46:54 -0700

    initial import of skeleton project

---
+++ AirForceOne/AirForceOnePlugin.h
@@ -0,0 +1,20 @@
+//
+//  AirForceOnePlugin.h
+//  AirForceOne
+//
+//  Created by Jean-Pierre Mouilleseaux on 7/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface AirForceOnePlugIn : QCPlugIn
+
+/*
+Declare here the properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property (copy) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
